f2d3899b0fe3dec0586b04741e9b7758fae5d38a
Didi0dum <dilyana.k.vasileva@gmail.com>
2026-02-09 22:23:23 +0200

    ☆ feat(coordinate conversion): helper function to convert spherical into cartesian coordinates

---
+++ utils/spherical_to_cartesian_coordinates.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def spherical_to_cartesian(dist, azimuth_deg, elevation_deg):
+    az = np.deg2rad(azimuth_deg)
+    el = np.deg2rad(elevation_deg)
+
+    x = dist * np.cos(el) * np.cos(az)
+    y = dist * np.cos(el) * np.sin(az)
+    z = dist * np.sin(el)
+
+    return np.array([x, y, z])
